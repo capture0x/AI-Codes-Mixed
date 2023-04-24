823ddaccb3d71ac81e16c8d9fc7d5c1e1a2af86a
capture0x <61238348+capture0x@users.noreply.github.com>
2023-04-24 19:06:35 -0400

    Update file

--- feel.py
+++ feel.py
@@ -1,9 +1,44 @@
+"""
+    Import necessary libraries:
+    The code imports OpenCV (cv2), TensorFlow (tf), and Numpy (np) libraries to use their functions for image processing and deep learning.
+
+    Load the pre-trained model:
+    The code loads a pre-trained deep learning model ('emotion_detection_model.h5') using the TensorFlow Keras API.
+
+    Load the face detection classifier:
+    The code loads the pre-trained Haar Cascade classifier ('haarcascade_frontalface_default.xml') using OpenCV. The classifier is used to detect faces in an image.
+
+    Start capturing frames from the webcam:
+    The code initializes the camera using OpenCV's VideoCapture() function and captures frames continuously.
+
+    Convert the captured frame to grayscale:
+    The code converts the captured frame to grayscale using OpenCV's cvtColor() function.
+
+    Detect faces in the grayscale image:
+    The code uses the face detection classifier to detect faces in the grayscale image using OpenCV's detectMultiScale() function.
+
+    Process each face detected:
+    For each face detected, the code extracts the region of interest (ROI) from the grayscale image and resizes it to 48x48 pixels. It also normalizes the pixel values and adds two additional dimensions to the tensor to match the input shape of the pre-trained model.
+
+    Make predictions using the pre-trained model:
+    The code uses the pre-trained model to make predictions on the processed image of the face.
+
+    Display the predicted emotion:
+    The code displays the predicted emotion as text on the frame using OpenCV's putText() function.
+
+    Display the resulting image:
+    The code displays the resulting image in a window using OpenCV's imshow() function.
+
+    Quit the program:
+    The code waits for a keyboard event to exit the program using OpenCV's waitKey() function.
+"""
+
 import cv2
 import tensorflow as tf
 import numpy as np
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-model = tf.keras.models.load_model('emotion_detection_model.h5')
+face_cascade = cv2.CascadeClassifier('/home/kali/data/haarcascades/haarcascade_frontalface_default.xml')
+model = tf.keras.models.load_model('/home/kali/Downloads/emotion_detection_model.h5')
 cap = cv2.VideoCapture(0)
 
 while True:
