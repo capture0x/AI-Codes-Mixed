6e111a64e55e0cd893f6c4d21e77d494c217757d
capture0x <61238348+capture0x@users.noreply.github.com>
2023-04-24 19:04:12 -0400

    UPLOAD

---
+++ feel.py
@@ -0,0 +1,29 @@
+import cv2
+import tensorflow as tf
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier('/home/kali/data/haarcascades/haarcascade_frontalface_default.xml')
+model = tf.keras.models.load_model('/home/kali/Downloads/emotion_detection_model.h5')
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+        roi_gray = cv2.resize(roi_gray, (48, 48))
+        roi_gray = roi_gray.astype('float') / 255.0
+        roi_gray = np.expand_dims(roi_gray, axis=0)
+        roi_gray = np.expand_dims(roi_gray, axis=-1)
+        prediction = model.predict(roi_gray)[0]
+        label = emotion_labels[prediction.argmax()]
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(frame, label, (x, y-10), font, 0.9, (0, 255, 0), 2, cv2.LINE_AA)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
